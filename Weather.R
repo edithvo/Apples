6d1f8e813d18f23165868f2e733dac062d68b099
edithvo <jane@example.com>
2021-10-21 12:03:12 +0200

    Commit 21-10-2021

---
+++ Weather.R
@@ -0,0 +1,31 @@
+getwd()
+setwd("C:/Users/dalha001/OneDrive - WageningenUR/Supervision/MSC/Edith van Oostrom/R/Apples")
+
+library(raster)
+
+raster
+
+test_tmin<-brick("weather data/tmin.2017.nc")
+
+plot(test_tmin[[365]])
+
+Wagen
+
+extract(test_tmin, SpatialPoints(cbind(5.673578442041631, 51.980419566974746)))
+spatial 
+
+SpatialPoints(cbind(51.980419566974746, 5.673578442041631))
+str(test_tmin)
+
+(test_tmin@data@min)
+
+
+list.files("weather data")
+
+for (i in 1:10){
+  print(i)
+}
+
+for (file in list.files("weather data")){
+  print(file)
+}
